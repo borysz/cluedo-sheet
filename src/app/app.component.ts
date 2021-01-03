3fbc8c39d4d60ac0204666ec884d9eb8252f4d41
Boros Norbert <norbi@bitninja.io>
2021-01-03 19:39:59 +0100

    add selectable markers

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,10 +11,11 @@ export class AppComponent {
   headerList = {};
   playersForm: FormGroup;  
   clickCount: number = 0; 
+  selectedMarker: string = 'x';
 
   constructor(){
     this.headerList = {
-      0: ['Mr. Green', 'Mustard', 'Orchid', 'Peacook', 'Pulm', 'Scarlett'],     
+      0: ['Mr. Green', 'Mustard', 'Orchid', 'Peacook', 'Plum', 'Scarlett'],     
       1: ['Bálterem', 'Billiárdszoba', 'Télikert', 'Ebédlő', 'Hall', 'Konyha', 'Könyvtár', 'Társalgó', 'Dolgozószoba'],
       2: ['Gyertyatartó', 'Tőr', 'Ólmosbot', 'Revolver', 'Szobor', 'Méreg'],
     }
@@ -33,20 +34,27 @@ export class AppComponent {
   onSubmitForm() {
   }
 
+  chooseMarker(marker){
+    this.selectedMarker = marker;
+  }
+
   changePlayerClick(event){      
     event.preventDefault();
-    
+
     this.clickCount++;
     setTimeout(() => {
         if (this.clickCount === 1) {
           event.target.classList.remove('c-img-q');
-          event.target.classList.toggle('c-img-x');
-        } else if (this.clickCount === 2) {
           event.target.classList.remove('c-img-x');
-          event.target.classList.toggle('c-img-q');
-        }
+          event.target.classList.remove('c-img-m'); 
+          event.target.classList.add('c-img-'+this.selectedMarker);   
+        } else if (this.clickCount === 2) {
+          event.target.classList.remove('c-img-q');
+          event.target.classList.remove('c-img-x');
+          event.target.classList.remove('c-img-m'); 
+        } 
         this.clickCount = 0;
-    }, 200)
+    }, 200) 
     
     //event.target.classList.add('c-img-x');     
     
